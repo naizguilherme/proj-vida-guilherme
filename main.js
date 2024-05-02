a17aa4ba34750dd067dfb48134a74501687c0039
Guilherme Naiz <108024770+naizguilherme@users.noreply.github.com>
2024-05-02 17:27:40 -0300

    02/05

--- main.js
+++ main.js
@@ -14,10 +14,12 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-07-08T00:00:00");
+const tempoObjetivo1 = new Date("2025-01-01T00:00:00");
+const tempoObjetivo2 = new Date("2025-01-01T00:00:00");
+const tempoObjetivo3 = new Date("2025-01-01T00:00:00");
+const tempoObjetivo4 = new Date("2025-01-01T00:00:00");
 
-
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
@@ -31,8 +33,24 @@ function calculaTempo(tempoObjetivo){
     minutos%=60;
     horas%=24;
 
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
 
+    if(tempoFinal>0){
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+    }
 
 
 }
+
+function atualizaCronometro(){
+    for( let i=0; i<contadores.length; i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000);
+}
+
+//comecaCronometro();
+
